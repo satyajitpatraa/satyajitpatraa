bf29ca15c71f539eb439f4b21f2269782fc194fd
satyajitpatraa <139449006+satyajitpatraa@users.noreply.github.com>
2025-02-16 19:42:14 +0530

    Add files via upload

---
+++ DSA in C/ANSWERS/ANSWERS/1. Basic/Q-2.c
@@ -0,0 +1,14 @@
+// c program to print your name 
+
+#include <stdio.h>
+
+int main() {
+    char name[50];  
+
+    printf("Enter your name: ");
+    scanf("%s", name); 
+
+    printf("Your name is: %s", name);  
+
+    return 0;
+}
